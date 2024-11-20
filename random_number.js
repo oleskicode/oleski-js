a6b030d860fe380016d574f6bcdb83287f8882ed
oleksii <plaunov@gmail.com>
2024-11-20 16:28:49 +0200

    random_number.js

---
+++ random_number.js
@@ -0,0 +1,11 @@
+let randomNum = Math.random()
+console.log(randomNum)
+
+let range = 5 // this sets [1...range]
+randomNum = Math.floor(Math.random() * range) + 1
+console.log("Random value from range [1,5]: " + randomNum)
+
+const MIN_RANGE_VALUE = 3;
+const MAX_RANGE_VALUE = 13;
+randomNum = Math.floor(Math.random()*(MAX_RANGE_VALUE-MIN_RANGE_VALUE+1) + MIN_RANGE_VALUE)
+console.log("Random value from range [3,13]: " + randomNum)
