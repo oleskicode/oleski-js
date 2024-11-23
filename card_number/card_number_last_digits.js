bb06f2002bdf2463f3ae441ae5251bff1760ea24
oleksii <plaunov@gmail.com>
2024-11-23 07:27:24 +0200

    card number last 4 digits exercise

---
+++ card_number/card_number_last_digits.js
@@ -0,0 +1,15 @@
+function maskCardNumber() {
+    const cardNumber = document.getElementById('cardNumber')
+    let lastFourDigits = cardNumber.value.slice(-4);
+    console.log("lastFourDigits = " + lastFourDigits);
+    console.log("getCardNumber(): cardNumber.value = " + cardNumber.value);
+    console.log("cardNumber.value.length = " + cardNumber.value.length)
+
+    let maskedCardNumber = ""
+    for (let i = 0; i < cardNumber.value.length-4; i++) {
+        maskedCardNumber += "*"
+    }
+    maskedCardNumber += lastFourDigits;
+    console.log(maskedCardNumber);
+    document.getElementById("maskedCardNumber").textContent = maskedCardNumber;
+}
