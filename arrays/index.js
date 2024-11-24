f262d776648f59b328e89f964170d4b5203a2b4e
oleksii <plaunov@gmail.com>
2024-11-24 15:50:19 +0200

    arrays

---
+++ arrays/index.js
@@ -0,0 +1,23 @@
+function arrayToSquare(inputArray) {
+    inputArray.forEach(element => {
+        element = element*element
+        console.log(element)
+    });
+}
+
+function arrayToSquareUsingFor(inputArray) {
+    console.log("arrayToSquareUsingFor")
+    for (let i = 0; i < inputArray.length; i++) {
+        inputArray[i] = inputArray[i]*inputArray[i]
+    }
+}
+
+array1 = [1,2,3]
+console.log(array1)
+
+arrayToSquare(array1)
+console.log(array1)
+
+arrayToSquareUsingFor(array1)
+console.log(array1)
+ 
