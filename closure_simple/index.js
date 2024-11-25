84d79bff570c16cca5882bedbcd5dd3272836809
oleksii <plaunov@gmail.com>
2024-11-25 22:53:48 +0200

    refactor

--- closure_simple/index.js
+++ closure_simple/index.js
@@ -2,14 +2,19 @@ const skyPosition = "The Sky is Above"; //global variable
 
 function skyStatus() {
     const moonVisibility = "The moon may be visible"; //local variable 
+    let counter = 0;
 
     return function() { //closure
         console.log("skyPosition " + skyPosition);
         console.log("moonVisibility " + moonVisibility);
+
+        counter++;
+        console.log(counter)
     }
 }
 
-skyStatus();
-
-const returnedFunction = skyStatus(); //getting returned function
-returnedFunction();
+skyStatus() //this does not affect output
+const getSkyStatusResult = skyStatus(); //getting returned function
+getSkyStatusResult();
+getSkyStatusResult();
+getSkyStatusResult();
