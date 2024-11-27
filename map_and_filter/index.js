5e450bc603733c7eed48c8e74bd4470f648f1ca9
oleksii <plaunov@gmail.com>
2024-11-27 16:13:11 +0200

    find method

--- map_and_filter/index.js
+++ map_and_filter/index.js
@@ -21,18 +21,24 @@ const edibles = [
     }
 ]
 
-console.log("Map function example") // Map
+console.log("Map method example") // Map
 const capitalizedEdibles = edibles.map(function (item) {
     return item = String(item.name[0]).toUpperCase() + String(item.name).slice(1);
 })
 console.log(capitalizedEdibles)
 
-console.log("Filter function example") // Filter
+console.log("Filter method example") // Filter
 console.log(typeof(edibles), edibles)
 const fruits = edibles.filter(function (item) {
     return (item.type === "fruit")
 });
 console.log(fruits)
 
-// console.log("Reduce function") //Reduce
-// console.log("Foreach function"); //Foreach
+console.log("Find method")
+const findBanana = edibles.find(function (item) {
+    return item.name === "banana"
+})
+console.log(findBanana)
+
+// console.log("Reduce method") //Reduce
+// console.log("Foreach method"); //Foreach
