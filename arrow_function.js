32701fc1a3a750fe36689b9ce9abbc57ccf8e113
Oleksii Plaunov <plaunov@gmail.com>
2025-01-02 15:43:35 +0200

    arrow function

---
+++ arrow_function.js
@@ -0,0 +1,7 @@
+const helloUserFoodReady = (userName, favFood) => {  // (parameters) => code
+    console.log(`Hello ${userName}`)
+    console.log(`Your order is ready: ${favFood}`)
+} 
+
+helloUserFoodReady("Tom", "pizza")
+helloUserFoodReady("Mark", "hamburger")
