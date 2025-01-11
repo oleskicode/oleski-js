40030cf8b8d8ee2527238fad12c4f7e1c04c0e68
Oleksii Plaunov <plaunov@gmail.com>
2025-01-11 19:34:36 +0200

    longest_substring

---
+++ longest_substring.js
@@ -0,0 +1,35 @@
+/* Find the Longest Substring Without Repeating Characters
+Given a string s, find the length of the longest substring without repeating characters.
+
+Input: s1 = "abcabcbb"
+Output: The answer is "abc", with the length of 3.
+
+Input: s2 = "bbbbb"
+Output: The answer is "b", with the length of 1. */
+
+s1 = "abcabcbb"
+s2 = "bbbbb"
+s3 = "1234123451234"
+
+function findLongestSubstring(str) {
+    console.log("Input: " + str)
+    arrayStr = str.split("")
+    console.log(arrayStr)
+    let biggestSubstring = ""
+    let currentSubstring = ""
+    arrayStr.forEach(char => { // check if char is in unique set
+        if (currentSubstring.includes(char)) { // "abc".includes("a"))
+            if (currentSubstring.length > biggestSubstring.length) {
+                biggestSubstring = currentSubstring
+            }
+        } else {
+            currentSubstring = currentSubstring + char
+        }
+    });
+
+    return `The answer is: ${biggestSubstring}, with the length of ${biggestSubstring.length}.`
+}
+
+console.log(findLongestSubstring(s1))
+console.log(findLongestSubstring(s2))
+console.log(findLongestSubstring(s3))
