24f6b998dc6509504512016a3e844bbd7cf1b9b5
Oleksii Plaunov <plaunov@gmail.com>
2025-03-13 23:03:38 +0200

    string challenge exercise

---
+++ string_challenge.js
@@ -0,0 +1,39 @@
+// Function which returns a new string by pairing up the characters in the corresponding locations in both substrings. 
+// str parameter being passed will contain a string with a single asterisk character splitting the string evenly into two separate strings. The string will always split evenly with the asterisk in the center.
+// For example: if str is "123*abc" then your program should return the string 1a2b3c because 1 pairs with a, 2 pairs with b, etc.
+// Input: "123*abc" >> Combined string: 1a2b3c
+// Once your function is working, take the final output string and concatenate it with your ChallengeToken, and then replace every fourth character with an underscore.
+// Your ChallengeToken: myr8iskabe
+// Final Output: 1a2_3cm_r8i_kab_
+
+let challengeToken = "myr8iskabe"
+
+function stringChallenge(inputString) {
+    let combinedString = ""
+    let input1 = inputString.split("*")[0]
+    let input2 = inputString.split("*")[1]
+
+    // pairing up characters
+    for (let i = 0; i < input1.length; i++) {
+        combinedString += input1[i] + input2[i]
+    }
+
+    // concatenate with challengeToken
+    combinedString += challengeToken 
+
+    //replace every fourth character with an underscore.
+    let finalString = ""
+    let counter = 1
+    combinedString.split("").forEach (element => {
+        if (counter % 4 === 0) {
+            finalString += "_"
+        } else {
+            finalString += element
+        }
+        counter++
+    });
+    
+    return finalString
+}
+
+console.log(stringChallenge("123*abc"))
