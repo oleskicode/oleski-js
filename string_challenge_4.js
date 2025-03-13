4006a97df25543c1e67fa72e79860bef6a5c6c3a
Oleksii Plaunov <plaunov@gmail.com>
2025-03-13 10:23:10 +0200

    String TODO

---
+++ string_challenge_4.js
@@ -0,0 +1,14 @@
+// Have the function StringChallenge (str)
+// take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna diroW olleH.
+// Once your function is working, take the final output string and concatenate it with your ChallengeToken, and then replace every fourth character with an underscore.
+// Your ChallengeToken: myr8iskabe
+// ?
+// Examples
+// Input: "coderbyte"
+// Output: etybredoc
+// Final Output: ety_red_cmy_8is_abe
+// Input: "I Love Code"
+// Output: edoC evol I
+// Final Output: edo_ ev_L I_yr8_ska_e
+
+// TODO
